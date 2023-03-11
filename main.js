1390a963a0e0a2d522c85b70280e0c04df503ee9
MisaMilly <97217666+MisaMilly@users.noreply.github.com>
2023-03-11 16:19:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function adduser() {
+    user_name = document.getElementById("user_name1").value;
+    localStorage.setItem("user_name", user_name);
+    window.location = "kwitter_room.html";
+var audio = new Audio("http://music.ogg");
+
+    var audio = document.getElementsByTagName("audio");
+    audio.play();
+  }
+
